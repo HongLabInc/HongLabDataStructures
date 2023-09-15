4dfa17c0ea9680d6dcce0058f01de340db048d48
HongLabInc <95195816+HongLabInc@users.noreply.github.com>
2023-09-14 22:22:30 -0700

    Ex0901 참고 주석 업데이트

--- Ex0901_BinarySearchTree/Ex0901_BinarySearchTree.cpp
+++ Ex0901_BinarySearchTree/Ex0901_BinarySearchTree.cpp
@@ -13,7 +13,7 @@ int main()
 	for (int i : { 5, 3, 7, 1, 4, 6, 8 })
 		//for (int i : { 9, 8, 7, 6, 5})
 	{
-		// bst.Insert(Item{ i, char('A' + i) });
+		// bst.Insert({ i, char('A' + i) }); // 참고: C++ 문법에서는 자료형의 이름 Item 생략하고 물결괄호 초기화 가능
 		bst.Insert(Item{ i, char('A' + i) });
 		bst.Print2D();
 	}
